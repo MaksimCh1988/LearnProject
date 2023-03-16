1559b5afb1b86a31c82168134d3100e9b534d942
MaksimCh1988 <maksim.ch@yahoo.com>
2023-03-16 19:49:49 +0300

    lesson20

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -4,22 +4,18 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categories': categories,
     }
     return render(request, 'news/index.html', context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
         'category': category,
-        'categories': categories,
     }
     return render(request, 'news/category.html', context)
